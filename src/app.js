7e0e885d2945731a901cbd4327748ecdf8a79a54
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-25 01:52:22 +0530

    Fixed cors error

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ const aiRoutes=require('./routes/ai.routes.js')
 const dotenv = require('dotenv');
 dotenv.config();
 
+const cors = require('cors');
+
 
 const app=express()
 app.use(express.json());
-
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
