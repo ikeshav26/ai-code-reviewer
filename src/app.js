17b8c145ad4348a9b447341f59adb40e61227587
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-25 09:41:45 +0530

    build update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app=express()
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://ai-code-reviewer-frontend-iota.vercel.app', 
   methods: ['GET', 'POST'],
   credentials: true
 }));
